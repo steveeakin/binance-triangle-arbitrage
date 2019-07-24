cfb362737acf6a1661c4a3ff29e8096c51bbe6b9
steveeakin <steven.eakin@gmail.com>
2019-07-24 09:03:18 -0400

    Fixed a possibility when the cron wouldnt respawn

--- cron.php
+++ cron.php
@@ -13,12 +13,9 @@ if (!empty($pid)) {
 	$pid = explode(' ', $pid);
 }
 
-if (file_exists('.start') && !(!empty($pid) && (!empty($pid[$_ENV['CRON_PID_NUMBER']]) && is_numeric($pid[$_ENV['CRON_PID_NUMBER']])))) {
+if (file_exists('.start') && !(!empty($pid) && (!empty($pid[$_ENV['CRON_PID_NUMBER']]) && is_numeric($pid[$_ENV['CRON_PID_NUMBER']])) && (stristr($pid[$_ENV['CRON_NODE_PID']], 'node') !== false))) {
 	// If .start exists, and pid is empty, launch.
-	if (stristr($pid[$_ENV['CRON_NODE_PID']], 'node') === false) {
-		// If this isn't running already.
-		$process = shell_exec($_ENV['LAUNCH_CODE']);
-	}
+	$process = shell_exec($_ENV['LAUNCH_CODE']);
 } elseif (!file_exists('.start') && (!empty($pid) && (!empty($pid[$_ENV['CRON_PID_NUMBER']]) && is_numeric($pid[$_ENV['CRON_PID_NUMBER']])))) {
 	// If start doesn't exist, and the process is running, kill the process.
 	shell_exec('kill -9 ' . $pid[$_ENV['CRON_PID_NUMBER']]);
