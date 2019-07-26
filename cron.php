1be8f5499b619e0d4f91bce908da6e865018958c
steveeakin <steven.eakin@gmail.com>
2019-07-26 15:10:40 -0400

    Fixed a bug that would cause the cron to do nothing if run in a crontab.  Kind of important.

--- cron.php
+++ cron.php
@@ -13,10 +13,10 @@ if (!empty($pid)) {
 	$pid = explode(' ', $pid);
 }
 
-if (file_exists('.start') && !(!empty($pid) && (!empty($pid[$_ENV['CRON_PID_NUMBER']]) && is_numeric($pid[$_ENV['CRON_PID_NUMBER']])) && (stristr($pid[$_ENV['CRON_NODE_PID']], 'node') !== false))) {
+if (file_exists(__DIR__ . '/.start') && !(!empty($pid) && (!empty($pid[$_ENV['CRON_PID_NUMBER']]) && is_numeric($pid[$_ENV['CRON_PID_NUMBER']])) && (stristr($pid[$_ENV['CRON_NODE_PID']], 'node') !== false))) {
 	// If .start exists, and pid is empty, launch.
 	$process = shell_exec($_ENV['LAUNCH_CODE']);
-} elseif (!file_exists('.start') && (!empty($pid) && (!empty($pid[$_ENV['CRON_PID_NUMBER']]) && is_numeric($pid[$_ENV['CRON_PID_NUMBER']])))) {
+} elseif (!file_exists(__DIR__ . '/.start') && (!empty($pid) && (!empty($pid[$_ENV['CRON_PID_NUMBER']]) && is_numeric($pid[$_ENV['CRON_PID_NUMBER']])))) {
 	// If start doesn't exist, and the process is running, kill the process.
 	shell_exec('kill -9 ' . $pid[$_ENV['CRON_PID_NUMBER']]);
 }
